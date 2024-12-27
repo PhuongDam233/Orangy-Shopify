a68963a9323434a47c9b36cf1f834734f6f111b8
PhuongDam233 <phuongdamthu233@gmail.com>
2024-12-27 13:40:32 +0700

    orangy-product-page: do faq for product

--- assets/bloglist.js
+++ assets/bloglist.js
@@ -2,7 +2,7 @@ function handleBlogList() {
   let tabs = document.querySelectorAll(".tag-btn-link");
 
   tabs.forEach(function (tab) {
-    tab.addEventListener("click", function () {
+    tab.addEventListener("click", function (event) {
       // remove all class active of tabs
       tabs.forEach(function (tab) {
         tab.classList.remove("active");
@@ -10,11 +10,7 @@ function handleBlogList() {
 
       //   add class active
       this.classList.add("active");
-
-      // get data-tab
-      let id = this.dataset.tab;
-      // addclass active to news list
-      document.querySelector("#" + id).classList.add("active");
     });
   });
 }
+handleBlogList();
